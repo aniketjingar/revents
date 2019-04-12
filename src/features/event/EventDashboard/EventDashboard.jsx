610af98afada872e399cc9031b05e48c377a7bb5
Aniket Jingar <aniketjingar@gmail.com>
2019-04-12 19:15:50 -0400

    section 8

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,63 +1,18 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Grid, Button } from "semantic-ui-react";
-import cuid from "cuid";
+import { Grid } from "semantic-ui-react";
 import EventList from "../EventList/EventList";
-import EventForm from "../EventForm/EventForm";
-import { createEvent, deleteEvent, updateEvent } from "../eventActions";
+import { deleteEvent } from "../eventActions";
 
 const mapState = state => ({
   events: state.events
 });
 
 const actions = {
-  createEvent,
-  deleteEvent,
-  updateEvent
+  deleteEvent
 };
 
 class EventDashboard extends Component {
-  state = {
-    isOpen: false,
-    selectedEvent: null
-  };
-
-  handleFormOpen = () => {
-    this.setState({
-      selectedEvent: null,
-      isOpen: true
-    });
-  };
-
-  handleCancel = () => {
-    this.setState({
-      isOpen: false
-    });
-  };
-
-  handleUpdateEvent = updatedEvent => {
-    this.props.updateEvent(updatedEvent);
-    this.setState({
-      isOpen: false,
-      selectedEvent: null
-    });
-  };
-
-  handleOpenEvent = eventToOpen => () => {
-    this.setState({
-      selectedEvent: eventToOpen,
-      isOpen: true
-    });
-  };
-
-  handleCreateEvent = newEvent => {
-    newEvent.id = cuid();
-    newEvent.hostPhotoURL = "/assets/user.png";
-    this.props.createEvent(newEvent);
-    this.setState({
-      isOpen: false
-    });
-  };
 
   handleDeleteEvent = eventId => () => {
     this.props.deleteEvent(eventId);
@@ -76,19 +31,6 @@ class EventDashboard extends Component {
           />
         </Grid.Column>
         <Grid.Column width={6}>
-          <Button
-            onClick={this.handleFormOpen}
-            positive
-            content="Create Event"
-          />
-          {this.state.isOpen && (
-            <EventForm
-              updateEvent={this.handleUpdateEvent}
-              selectedEvent={this.state.selectedEvent}
-              createEvent={this.handleCreateEvent}
-              handleCancel={this.handleCancel}
-            />
-          )}
         </Grid.Column>
       </Grid>
     );
--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -1,18 +1,20 @@
-import React, { Component } from 'react'
-import EventListItem from './EventListItem'
+import React, { Component } from "react";
+import EventListItem from "./EventListItem";
 
- class EventList extends Component {
+class EventList extends Component {
   render() {
-    const {events, onEventOpen, deleteEvent} = this.props;
+    const { events, deleteEvent } = this.props;
     return (
       <div>
-        <h1>EventList</h1>
-        {events.map((event) => (
-          <EventListItem key={event.id} onEventOpen={onEventOpen} deleteEvent={deleteEvent} event={event}/>
+        {events.map(event => (
+          <EventListItem
+            key={event.id}
+            deleteEvent={deleteEvent}
+            event={event}
+          />
         ))}
-        
       </div>
-    )
+    );
   }
 }
-export default EventList
+export default EventList;

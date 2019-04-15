a3e87fc9bd1189d1ccf3414b7a78fe3e7e1ea818
Aniket Jingar <aniketjingar@gmail.com>
2019-04-15 16:59:00 -0400

    Section 11

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,19 +1,27 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Button } from 'semantic-ui-react'
+import { openModal } from "../modals/modalActions";
 
-const mapState = (state) => ({
-    data: state.test.data
-})
+const mapState = state => ({
+  data: state.test.data
+});
+
+const actions = {
+  openModal
+}
 
 class TestComponent extends Component {
   render() {
+    const { openModal } = this.props;
     return (
       <div>
         <h1>Test Area</h1>
         <h3>The answer is: {this.props.data}</h3>
+        <Button onClick={() => openModal('TestModal', {data: 43})} color='teal' content='Open Modal' />
       </div>
-    )
+    );
   }
 }
 
-export default connect(mapState)(TestComponent)
+export default connect(mapState, actions)(TestComponent);
